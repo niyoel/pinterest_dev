978ab9fdf7964440f61c9e4a220c76caef505abf
niyobuhungiro ellen <ellenniyobuhungiro01@gmail.com>
2022-05-03 09:24:14 +0200

    pinterest_dev commit

---
+++ src/Controller/PinController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinController extends AbstractController
+{
+     #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return new Response("Hello World");
+        // return $this->json(['message' => 'Hello World']);
+        // { return $this->render('pin/index.html.twig', [
+        //     'controller_name' => 'PinController',
+        // ]); }
+    }
+}
