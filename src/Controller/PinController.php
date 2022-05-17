171136003cca47b198d951af4fa0277c4c18fa85
niyobuhungiro ellen <ellenniyobuhungiro01@gmail.com>
2022-05-17 15:52:43 +0200

    creation de page

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -7,43 +7,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Form\PinType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+
 
 class PinController extends AbstractController
 {
      #[Route('/', name: 'app_home')]
      public function index(ManagerRegistry $doctrine, PinRepository $repo): Response
      {
-         $pin = new Pin;
-         $pin->setTitle("Pin ");
-         $pin->setDescription("Description Pin ");
-         
-         $pin1 = new Pin;
-         $pin1->setTitle("Pin 2");
-         $pin1->setDescription("Description Pin 2");
-        
-         $pin2 = new Pin;
-         $pin2->setTitle("Pin 2");
-         $pin2->setDescription("Description Pin 2");
-         
-
-         $pin3 = new Pin;
-         $pin3->setTitle("Pin 3");
-         $pin3->setDescription("Description Pin 13");
-
-         $pin4 = new Pin;
-         $pin4->setTitle("Pin 4");
-         $pin4->setDescription("Description Pin 4");
-
-         $em = $doctrine->getManager();
-         $em->persist($pin);
-         $em->persist($pin1);
-         $em->persist($pin2);
-         $em->persist($pin3);
-         $em->persist($pin4);
-
-         $em->flush(); 
-        
-         
          return $this->render('pin/index.html.twig', ['pins' => $repo->findAll()]);
      }
+    #[Route('/pin/{id<[0-9]+>}', name:'app_pin_show', methods:'GET')]
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pin/show.html.twig', compact('pin'));
+    }    
+    #[Route("/pin/create", name:"app_pin_create", methods: ["GET","POST"])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $pin = new Pin;
+        $form = $this->createForm(PinType::class, $pin);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($pin);
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('pin/create.html.twig', ['monForm' => $form->createView()]);   
+       }
 }
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace App\Entity;
-
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
-
 #[ORM\Entity(repositoryClass: PinRepository::class)]
 #[ORM\Table(name:"pins")]
 #[ORM\HasLifecycleCallbacks]
